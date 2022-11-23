a7610460df227f96bf0a806a7c232dd25e231ec2
yusufcanbircan <yusufcannnz@hotmail.com>
2022-11-23 21:19:44 +0300

    design done

---
+++ FlagQuizApp/FlagQuizApp/QuizScreenViewController.swift
@@ -0,0 +1,19 @@
+//
+//  QuizScreenViewController.swift
+//  FlagQuizApp
+//
+//  Created by Yusuf Can Bircan on 23.11.2022.
+//
+
+import UIKit
+
+class QuizScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}

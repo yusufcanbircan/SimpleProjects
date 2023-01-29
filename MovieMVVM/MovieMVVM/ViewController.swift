30d4ed5d5abce03bbebfcfc40bd0fa05a6fcfeeb
yusufcanbircan <yusufcannnz@hotmail.com>
2023-01-29 17:01:31 +0300

    first commit

---
+++ MovieMVVM/MovieMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieMVVM
+//
+//  Created by Yusuf Can Bircan on 29.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
